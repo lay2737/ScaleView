9a25b45879e8c8b7251057dbf7e8a410cce2876d
刘恒 <lay2737@outlook.com>
2016-05-30 17:09:58 +0800

    仿贴吧签到视图

--- ScaleView.swift
+++ ScaleView.swift
@@ -11,67 +11,77 @@ import UIKit
 class ScaleView: UIView ,UICollectionViewDelegate ,UICollectionViewDataSource{
     
     
-    
-    var dateArray = [["2.8","2.10"],["2.17","2.20"],["2.22","2.24"],["3.1","3.8"],["",""]]
+    //传入签到日期数组
+    var dateArray = [["2.8","2.10"],["2.17","2.20"],["2.22","2.24"],["3.1","3.8"]]
     
     lazy var collectionView:UICollectionView = {
         let flowLayout:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         flowLayout.itemSize = CGSizeMake(self.frame.size.width/3*2, self.frame.size.height/2)
-        flowLayout.footerReferenceSize = CGSize(width:self.frame.size.width/3*2, height:self.frame.size.height/2)
+        flowLayout.footerReferenceSize = CGSize(width:self.frame.size.width/3, height:self.frame.size.height/2)
         flowLayout.headerReferenceSize = CGSize(width:1, height:1)
         flowLayout.scrollDirection = UICollectionViewScrollDirection.Horizontal
         flowLayout.minimumLineSpacing = 0
         
         let collection:UICollectionView = UICollectionView(frame: CGRectMake(0, self.frame.size.height/2, self.frame.size.width, self.frame.size.height/2), collectionViewLayout: flowLayout)
         collection.registerNib(UINib.init(nibName: "ScaleCell", bundle: nil), forCellWithReuseIdentifier: "cell")
-        
-        collection.registerClass(FooterView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "footCell")
-      //  collection.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "headCell")
-       // collection.registerNib(UINib.init(nibName: "ScaleCell",bundle:nil), forSupplementaryViewOfKind: "foot", withReuseIdentifier: "FootCell")
-        // collectionView.registerClass(UICollectionViewCell().classForCoder, forCellWithReuseIdentifier: "cell")
+        collection.registerNib(UINib.init(nibName: "FooterView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "footCell")
+       
         collection.delegate = self
         collection.dataSource = self
         collection.showsHorizontalScrollIndicator = false
         collection.bounces = false
-
+        
         self.addSubview(collection)
         
         //default
         collection.contentOffset = CGPoint(x:flowLayout.itemSize.width*2, y:0)
         collection.contentSize = CGSize(width: 200, height: 50)
-
+        
         print( flowLayout.collectionViewContentSize())
         return collection
     }()
     
-   
+    
     var signLable:UILabel!
     override init(frame: CGRect) {
         super.init(frame: frame)
         signLable = UILabel.init(frame: CGRectMake(self.frame.size.width/2-100, self.frame.size.height/4, 200, self.frame.size.height/4))
-        signLable.text = "连续签到 8 天"
+        signLable.text = "连续签到 1 天"
         signLable.font = UIFont.systemFontOfSize(22)
         signLable.textColor = LExtension.RGBColor(0xFFFCE2)
         signLable.textAlignment = NSTextAlignment.Center
         self.addSubview(signLable)
         setUp()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         fatalError("init(coder:) has not been implemented")
     }
-   
+    
     func setUp(){
         collectionView.backgroundColor = UIColor.clearColor()
         
-        
+        //渐变色
         let gradient = CAGradientLayer.init()
         gradient.frame = self.bounds
         gradient.colors = [LExtension.RGBColor(0xFF6778).CGColor,LExtension.RGBColor(0x96A0E8).CGColor]
         self.layer.insertSublayer(gradient, atIndex: 0)
-    
         
+        
+        
+        let time = dateArray.last?.last
+        let dateForm = NSDateFormatter()
+        dateForm.dateFormat = "MM.dd"
+        let date = dateForm.dateFromString(time!)
+        
+        let now = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let flags: NSCalendarUnit = .Day
+        let nowCom = calendar.components(flags, fromDate: now)
+        let timeCom = calendar.components(flags, fromDate: date!)
+        signLable.text = "连续签到 \(nowCom.day - timeCom.day) 天"
+
     }
     
     
@@ -81,26 +91,30 @@ class ScaleView: UIView ,UICollectionViewDelegate ,UICollectionViewDataSource{
         //cell.drawLine()
         cell.startLabel.text = dateArray[indexPath.row][0]
         cell.endLabel.text = dateArray[indexPath.row][1]
-     //   cell.backgroundColor =  indexPath.row%2==0 ? UIColor.blackColor():UIColor.whiteColor()
+        //   cell.backgroundColor =  indexPath.row%2==0 ? UIColor.blackColor():UIColor.whiteColor()
         return cell
     }
     func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath)  {
-      // ( cell as! ScaleCell) .drawLine()
+        // ( cell as! ScaleCell) .drawLine()
     }
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return dateArray.count
     }
-//    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-//        var v = FooterView()
-//        if kind == UICollectionElementKindSectionFooter{
-//            
-//          v = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionFooter, withReuseIdentifier: "footCell", forIndexPath: indexPath) as! FooterView
-//        }
-//        return v
-//
-//    }
-//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize{
-//            
-//            return CGSize(width: self.bounds.width/3*2, height: self.bounds.width/2)
-//    }
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        var v = FooterView()
+        if kind == UICollectionElementKindSectionFooter{
+            
+            v = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionFooter, withReuseIdentifier: "footCell", forIndexPath: indexPath) as! FooterView
+            print("foot")
+        }
+        return v
+        
+        
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize{
+        return CGSizeZero;
+        //return CGSize(width: self.bounds.width/3*2, height: self.bounds.width/2)
+    }
+  
 }
--- ScaleView/FooterView.swift
+++ ScaleView/FooterView.swift
@@ -9,5 +9,31 @@
 import UIKit
 
 class FooterView: UICollectionReusableView {
+    let radius:CGFloat = 5.0
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    override func awakeFromNib() {
+        let currenDay = NSDate()
+        let dateForm = NSDateFormatter()
+        dateForm.dateFormat = "M.dd"
+        self.dateLabel.text = dateForm.stringFromDate(currenDay)
+    }
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
         
+        let context:CGContextRef = UIGraphicsGetCurrentContext()!
+        
+        CGContextSetRGBStrokeColor(context, 255/255, 252/255, 225/255, 1)
+        CGContextSetLineWidth(context, radius/3)
+        CGContextAddLines(context, [CGPoint(x:0, y:self.frame.size.height/2),CGPoint(x:self.frame.size.width/3 - radius, y:self.frame.size.height/2)], 2)
+        CGContextDrawPath(context, CGPathDrawingMode.FillStroke)
+        
+        //画实心圆
+        // CGContextSetRGBStrokeColor(context, 0, 0, 255, 1);
+        CGContextSetLineWidth(context, 5.0);
+        CGContextAddEllipseInRect(context, CGRectMake(self.frame.size.width/3 - radius, self.frame.size.height/2 - radius,2*radius, 2*radius));//在这个框中画圆'
+        CGContextSetFillColor(context, CGColorGetComponents( LExtension.RGBColor(0xFFFCE2) .CGColor) )
+        CGContextFillPath(context)
+        
+    }
 }
